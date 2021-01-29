485283688245914a60bc5f8c7fab1790514319fd
SMKrizan <smkrizan@gmail.com>
2021-01-29 12:41:16 -0600

    static nav setup

---
+++ src/assets/images/photos.js
@@ -0,0 +1,66 @@
+import { TallyMax, wishList, weatherView, onBase, readTheory, eCommBackEnd } from '../../assets/images';
+
+const photos = [
+    {
+        src: { TallyMax },
+        // thumbnail: './TallyMax.png',
+        width: 1,
+        height: 1,
+        alt: 'TallyMax application screenshot',
+        // tags: [{ value: "Javascript", title: "Javascript" }],
+        // caption: 'Group Project: "Tally Max" word generator (https://github.com/jgrossh2/word-generator)',
+    },
+    {
+        // src: 'https://twelve-wishes.herokuapp.com/',
+        src: { wishList },
+        width: 1,
+        height: 1,
+        alt: '12Wishes application screenshot',
+        // tags: [{ value: "Javascript", title: "Javascript" },
+        // { value: 'Handlebars', title: 'Handlebars' },
+        // { value: 'SCSS', title: 'SCSS' }],
+        // caption: 'Group Project: "12Wishes" holiday wish lists (https://github.com/chriswein12/wish-list)',
+    },
+    {
+        // src: 'https://smkrizan.github.io/WeatherView/',
+        src: { weatherView },
+        width: 2,
+        height: 1,
+        alt: 'weatherView application screenshot',
+        // tags: [{ value: "Javascript", title: "Javascript" },
+        // { value: 'HTML', title: 'HTML' },
+        // { value: 'CSS', title: 'CSS' }],
+        // caption: '"WeatherView" access current & forecasted weather using 3rd party API (https://github.com/smkrizan/WeatherView/)',
+    },
+    {
+        // src: 'https://github.com/SMKrizan/onBase',
+        src: { onBase },
+        width: 1,
+        height: 1,
+        alt: 'onBase application screenshot',
+        // tags: [{ value: "Javascript", title: "Javascript" }],
+        // caption: '"onBase" CLI-app/CMS for tracking employee data',
+    },
+    {
+        // src: 'https://readtheory.herokuapp.com/',
+        src: { readTheory },
+        width: 1,
+        height: 1,
+        alt: 'readTheory application screenshot',
+        // tags: [{ value: "Javascript", title: "Javascript" },
+        // { value: 'HTML', title: 'HTML' },
+        // { value: 'CSS', title: 'CSS | HTML' }],
+        // caption: '"readTheory" note taking app (https://github.com/SMKrizan/readTheory)',
+    },
+    {
+        // src: 'https://github.com/SMKrizan/E-Commerce-Back-End',
+        src: { eCommBackEnd },
+        width: 1,
+        height: 1,
+        alt: 'E-Commerce back end application screenshot',
+        // tags: [{ value: "Javascript", title: "Javascript" }],
+        // caption: 'E-Commerce Back End',
+    }
+];
+
+export default photos;
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,16 +1,27 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
-const Nav = () => {
+const Nav = (props) => {
+    
+    const { 
+        categories = [],
+        currentCategory,
+        setCurrentCategory,
+    } = props;
+
+    
     return (
-        <ul>
+        <ul className='flex-row'>
             <li>
-                <a href="#about">About</a>
+                <a href="#about" >About me</a>
             </li>
             <li>
-                <a href="#contact">Contact</a>
+                <a href="#projects">Featured Projects</a>
             </li>
             <li>
-                <a href="#portfolio">Portfolio</a>
+                <a href="#contact">Get in Touch</a>
+            </li>
+            <li>
+                <a href="#resume">Resume</a>
             </li>
         </ul>
 
