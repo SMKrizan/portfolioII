d035a04bf9eadae506c5b31663b62758e41cc0fb
SMKrizan <smkrizan@gmail.com>
2021-02-28 18:56:50 -0600

    updated image folders

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,19 +1,19 @@
 // for deploying to gh-pages, works as 'images...'; 
 // for localhost works with /assets... and portfolioII/assets...
 // for heroku
-const GitHub = '/assets/images/GitHub.png';
-const keep = '/assets/images/keep.jpg';
-const portrait = '/assets/images/portrait.jpeg';
-const LIBug = '/assets/images/LIBug.png';
-const onBase = '/assets/images/onBase.jpg';
-const readTheory = '/assets/images/readTheory.jpg';
-const shopShop = '/assets/images/shopShop.jpg';
-const stack = '/assets/images/stack.png';
-const res = '/assets/images/res.jpg';
-const TallyMax = '/assets/images/TallyMax.png';
-const weatherView = '/assets/images/weatherView.png';
-const woodlandConsulting = '/assets/images/wc.jpg'
-const wishList = '/assets/images/wishList.png';
+const GitHub = './assets/images/GitHub.png';
+const keep = './assets/images/keep.jpg';
+const portrait = './assets/images/portrait.jpeg';
+const LIBug = './assets/images/LIBug.png';
+const onBase = './assets/images/onBase.jpg';
+const readTheory = './assets/images/readTheory.jpg';
+const shopShop = './assets/images/shopShop.jpg';
+const stack = './assets/images/stack.png';
+const res = './assets/images/res.jpg';
+const TallyMax = './assets/images/TallyMax.png';
+const weatherView = './assets/images/weatherView.png';
+const woodlandConsulting = './assets/images/wc.jpg'
+const wishList = './assets/images/wishList.png';
 
 export default {
     GitHub,
