02d8d160463f8d5840e49e1cc8c16d6da12c3f15
SMKrizan <smkrizan@gmail.com>
2021-03-07 18:28:43 -0600

    grid applied to about page - MQs yet-to-go

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -6,75 +6,63 @@ const { portrait, LIBug, GitHub, stack } = images;
 
 const About = () => {
   return (
-    <section className="about">
-      <h1 className="about">
+    <section className="about page">
+      <h1 className="about title">
         <span className="reg">About </span>
         <span className="bold">me</span>
       </h1>
-      <div className="about">
-        <div className="me">
-          <img
-            src={portrait}
-            alt="photographic portrait of Sara Krizan"
-            className="me"
-          />
+      <div className="about portrait">
+        <img
+          src={portrait}
+          alt="photographic portrait of Sara Krizan"
+          className="me"
+        />
+      </div>
+      <div className="about content">
+        <h2 className="about">
+          My name is Sara.
+        </h2>
+        <p className="about">
+          I am a full stack web developer and project manager with over a decade
+          of experience providing client and administrative support for legal,
+          scientific, and not-for-profit businesses. I am interested in helping
+          your teams find elegant solutions to challenges in software
+          development and project management.
+        </p>
+      </div>
+      <div className="about links">
+        <div className="badge-container li">
+          <a className="about" id="li"
+            href="https://www.linkedin.com/in/smkrizan/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img
+              src={LIBug}
+              alt="https://www.linkedin.com/in/smkrizan/"
+            />
+          </a>
         </div>
-        <div id="about-text-container">
-          <div className="about-text">
-            <h2 className="about">
-              <span>My name is</span> Sara.
-            </h2>
-            <p className="about">
-              I am a full stack web developer and project manager with over a
-              decade of experience providing client and administrative support
-              for legal, scientific, and not-for-profit businesses. I am
-              interested in helping your teams find elegant solutions to
-              challenges in software development and project management.
-            </p>
-          </div>
-          <div id="links">
-            <div>
-              <div className="badge" id="li">
-                <a
-                  href="https://www.linkedin.com/in/smkrizan/"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <img
-                    id="li"
-                    src={LIBug}
-                    alt="https://www.linkedin.com/in/smkrizan/"
-                  />
-                </a>
-              </div>
-            </div>
-            <div>
-              <div className="badge" id="gh">
-                <a
-                  href="https://github.com/SMKrizan"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <img id="gh" src={GitHub} alt="https://github.com/SMKrizan" />
-                </a>
-              </div>
-            </div>
-            <div>
-              <div className="badge" id="so">
-                <a
-                  href="https://stackoverflow.com/users/14132348/smkrizan?tab=profile"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <img
-                    id="so"
-                    src={stack}
-                    alt="https://stackoverflow.com/users/14132348/smkrizan?tab=profile"
-                  />
-                </a>
-              </div>
-            </div>
-          </div>
+        <div className="badge-container gh">
+          <a className="about" id="gh"
+            href="https://github.com/SMKrizan"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img src={GitHub} alt="https://github.com/SMKrizan" />
+          </a>
+        </div>
+        <div className="badge-container so">
+          <a className="about" id="so"
+            href="https://stackoverflow.com/users/14132348/smkrizan?tab=profile"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img
+              src={stack}
+              alt="https://stackoverflow.com/users/14132348/smkrizan?tab=profile"
+            />
+          </a>
         </div>
       </div>
     </section>
