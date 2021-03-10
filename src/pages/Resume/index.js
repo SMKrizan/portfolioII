01b7ca26df0e9fb505204cb929910b054648cebe
SMKrizan <smkrizan@gmail.com>
2021-03-10 17:54:58 -0600

    duplicate rendering issue

--- src/pages/Resume/index.js
+++ src/pages/Resume/index.js
@@ -1,14 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
+import { Document, Page, pdfjs } from "react-pdf";
+import smkres from "../../utils/smkres.pdf";
 import "./index.css";
-import PDFViewer from "../../components/PDFViewer";
 
-const Resume = () => {
+pdfjs.GlobalWorkerOptions.workerSrc = `https://cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
+
+function Resume() {
+  const [numPages, setNumPages] = useState(null);
+  const [pageNumber, setPageNumber] = useState(1);
+
+  function onDocumentLoadSuccess({ numPages }) {
+    setNumPages(numPages);
+    setPageNumber(1);
+  }
+
+  function changePage(offset) {
+    setPageNumber((prevPageNumber) => prevPageNumber + offset);
+  }
+
+  function previousPage() {
+    changePage(-1);
+  }
+
+  function nextPage() {
+    changePage(1);
+  }
 
   return (
-    <div className="Resume">
-      <PDFViewer />
+    <div>
+      <Document file={smkres} onLoadSuccess={onDocumentLoadSuccess}>
+        <Page pageNumber={pageNumber} />
+      </Document>
+      <div>
+        <p>
+          Page {pageNumber} of {numPages}
+        </p>
+        <button type="button" disabled={pageNumber <= 1} onClick={previousPage}>
+          Previous
+        </button>
+        {"    "}
+        <button
+          type="button"
+          disabled={pageNumber >= numPages}
+          onClick={nextPage}
+        >
+          Next
+        </button>
+      </div>
     </div>
   );
-};
+}
 
 export default Resume;
+
