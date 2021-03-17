f3da2a5c4588939837c48436568b32fd1c990449
SMKrizan <smkrizan@gmail.com>
2021-03-17 16:22:29 -0500

    cards looking fine at all mq

--- src/pages/Projects/index.js
+++ src/pages/Projects/index.js
@@ -13,19 +13,17 @@ const Projects = () => {
       </h1>
       {project_info.map((project) => (
         <div key={project.key} className="card-container">
-          <h3>{project.title}</h3>
           {/* <div
             alt={project.alt} 
             className={project.name}
             style={{ backgroundImage: "url(" + project.src + ")" }}
             ></div> */}
+          <h3>{project.title}</h3>
+          <img className="card" src={project.src} alt={project.alt} />
           <p>{project.description}</p>
-          <img className="card" src={project.src} alt={project.alt}/>
-          <div className="links">
+          <div className="project-links">
             <a href={project.repository}>Repo</a>
-            {project.deployment ? (
-              <a href={project.deployment}>App</a>
-            ) : null}
+            {project.deployment ? <a href={project.deployment}>App</a> : null}
           </div>
         </div>
       ))}
